7cefa9a624eb0896a8a6622b7535fe46bcdae2bf
Devin Jacobs <dbuttonz@gmail.com>
2020-02-12 17:53:54 -0700

    Distributed gengames.jl

--- gengames.jl
+++ gengames.jl
@@ -1,15 +1,18 @@
-using GameRunner
-using JLD
-using MCPlayerM
-using Printf
-using ProgressMeter
-using Serialization
+using Distributed
 
-@showprogress 1 "Playing games..." for i in 1:parse(Int, ARGS[1])
+@everywhere using Pkg
+@everywhere Pkg.activate(".")
+@everywhere using GameRunner
+@everywhere using JLD
+@everywhere using MCPlayerM
+@everywhere using Printf
+@everywhere using ProgressMeter
+
+@showprogress pmap((i, ARGS[2]) for i in 1:parse(Int, ARGS[1])) do (i, save_to)
     game_record = play_game(
         MCPlayer(),
         GameRunner.RandomPlayerM.RandomPlayer(),
         board_size=9,
         quiet=true)
-    save(@sprintf("%s-%06d.jld", ARGS[2], i), "game_record", game_record)
+    save(@sprintf("%s-%04d.jld", save_to, i), "game_record", game_record)
 end
